43eb5fd42f3acca5ce5a359978378da5db5b378d
Frank Caron <frank@frankcaron.com>
2020-12-20 12:04:26 -0500

    Made card fully dynamic

--- force-app/main/default/lwc/propertyListView/propertyListView.js
+++ force-app/main/default/lwc/propertyListView/propertyListView.js
@@ -1,11 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
 export default class PropertyListView extends LightningElement {
 
     //Variables
+    @api
     properties = [
-        { id: 123, title: "AVA 55 Ninth", studio: 500, oneBed: 1000, twoBed: 2000, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
-        { id: 234, title: "AVA Sunset", studio: 500, oneBed: 1000, twoBed: 2000, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
+        { id: 123, title: "AVA 55 Ninth", description: "This is a property description.", offers: 2, studio: 500, oneBed: 1000, twoBed: 2000, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
+        { id: 234, title: "AVA Sunset", description: "This is another property description.", offers: 1, studio: 800, oneBed: 1200, twoBed: 1900, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
     ];
+
+    //@api
+    //properties;
 
 }
