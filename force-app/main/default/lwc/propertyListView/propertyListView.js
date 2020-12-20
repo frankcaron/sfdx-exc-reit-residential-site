775584e4c63d3bcff3ffe3506207a3269af12ebf
Frank Caron <frank@frankcaron.com>
2020-12-20 12:02:16 -0500

    Updated styling

--- force-app/main/default/lwc/propertyListView/propertyListView.js
+++ force-app/main/default/lwc/propertyListView/propertyListView.js
@@ -4,8 +4,8 @@ export default class PropertyListView extends LightningElement {
 
     //Variables
     properties = [
-        { id: 123, title: "AVA 55 Ninth", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
-        { id: 234, title: "AVA Sunset", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
+        { id: 123, title: "AVA 55 Ninth", studio: 500, oneBed: 1000, twoBed: 2000, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
+        { id: 234, title: "AVA Sunset", studio: 500, oneBed: 1000, twoBed: 2000, img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
     ];
 
 }
