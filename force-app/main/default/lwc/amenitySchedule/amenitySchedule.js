9ac973fabef39b83be0b994a4e69e48d596af836
Frank Caron <frank@frankcaron.com>
2020-12-20 14:36:33 -0500

    Added selection capability

--- force-app/main/default/lwc/amenitySchedule/amenitySchedule.js
+++ force-app/main/default/lwc/amenitySchedule/amenitySchedule.js
@@ -1,6 +1,22 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AmenitySchedule extends LightningElement {
+
+    //Properties
     @api
     recordId;
+
+    //Variables
+    selectedElement;
+
+    // Toggle Select
+    toggleSelection(event){
+        //Remove old class
+        if (this.selectedElement) { this.selectedElement.classList.toggle('selected') };
+
+        //Add new class
+        let evt = event.currentTarget;
+        evt.classList.toggle('selected');
+        this.selectedElement = evt;
+    }
 }
