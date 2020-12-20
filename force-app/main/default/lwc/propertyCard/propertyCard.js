814185f6ba5b4acc197f1e0d29ec4149f830a407
Frank Caron <frank@frankcaron.com>
2020-12-20 10:54:54 -0500

    Built out component nesting

--- force-app/main/default/lwc/propertyCard/propertyCard.js
+++ force-app/main/default/lwc/propertyCard/propertyCard.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class PropertyCard extends LightningElement {}
+export default class PropertyCard extends LightningElement {
+
+    //Variables
+    @api
+    property;
+
+}
--- force-app/main/default/lwc/propertyListView/propertyListView.js
+++ force-app/main/default/lwc/propertyListView/propertyListView.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class PropertyListView extends LightningElement {}
+export default class PropertyListView extends LightningElement {
+
+    //Variables
+    properties = [
+        { id: 123, title: "test"},
+        { id: 234, title: "behest"}
+    ];
+
+}
