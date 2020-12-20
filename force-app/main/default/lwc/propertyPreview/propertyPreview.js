6dca0670107342b217b179d9c4f650383093e80a
Frank Caron <frank@frankcaron.com>
2020-12-20 13:44:39 -0500

    Updated property preview component

--- force-app/main/default/lwc/propertyPreview/propertyPreview.js
+++ force-app/main/default/lwc/propertyPreview/propertyPreview.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import IMAGE_FIELD from '@salesforce/schema/Rental_Property__c.Image__c';
 
-export default class PropertyPreview extends LightningElement {}
+export default class PropertyPreview extends LightningElement {
+    @api
+    recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields: [IMAGE_FIELD] })
+    image;
+
+    get imagePath() {
+        return getFieldValue(this.image.data, IMAGE_FIELD);
+    }
+}
