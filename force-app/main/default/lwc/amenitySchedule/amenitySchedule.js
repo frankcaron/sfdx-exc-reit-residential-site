b2bf6fc28dfd372d8923357f8400023bb47ab638
Frank Caron <frank@frankcaron.com>
2020-12-20 15:31:43 -0500

    Filled out form

--- force-app/main/default/lwc/amenitySchedule/amenitySchedule.js
+++ force-app/main/default/lwc/amenitySchedule/amenitySchedule.js
@@ -1,5 +1,4 @@
 import { LightningElement, api } from 'lwc';
-
 export default class AmenitySchedule extends LightningElement {
 
     //Properties
@@ -8,15 +7,33 @@ export default class AmenitySchedule extends LightningElement {
 
     //Variables
     selectedElement;
+    selectedTime;
 
     // Toggle Select
     toggleSelection(event){
-        //Remove old class
-        if (this.selectedElement) { this.selectedElement.classList.toggle('selected') };
 
-        //Add new class
+        //Get new event
         let evt = event.currentTarget;
-        evt.classList.toggle('selected');
-        this.selectedElement = evt;
+
+        //If an event is already selected before,
+        if (this.selectedElement) {
+            this.selectedElement.classList.toggle('selected');
+            this.selectedElement = evt;
+            this.selectedElement.classList.toggle('selected');
+            this.selectedTime = this.selectedElement.getAttribute('data-appointmenttime');
+        } else {
+            this.selectedElement = evt;
+            this.selectedElement.classList.toggle('selected');
+            this.selectedTime = this.selectedElement.getAttribute('data-appointmenttime');
+        }
+
+        // Logging
+        console.log("REIT Amenity Schedule || Selected element: " + this.selectedElement);
+        console.log("REIT Amenity Schedule || Selected Time: " + this.selectedTime);
+    }
+
+    // Create Record
+    createAppointment() {
+        //Create appointment
     }
 }
