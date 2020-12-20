0805b1303528e219143d8a8135c09c941866bac5
Frank Caron <frank@frankcaron.com>
2020-12-20 11:18:15 -0500

    Added animation

--- force-app/main/default/lwc/propertyListView/propertyListView.js
+++ force-app/main/default/lwc/propertyListView/propertyListView.js
@@ -4,8 +4,8 @@ export default class PropertyListView extends LightningElement {
 
     //Variables
     properties = [
-        { id: 123, title: "test", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
-        { id: 234, title: "behest", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
+        { id: 123, title: "AVA 55 Ninth", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"},
+        { id: 234, title: "AVA Sunset", img: "https://media.gettyimages.com/photos/large-multicondos-building-blocks-with-bicycles-lane-picture-id1174752803?s=612x612"}
     ];
 
 }
