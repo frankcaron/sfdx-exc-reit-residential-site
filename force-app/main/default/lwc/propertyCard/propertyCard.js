0ed3759c0b37a70fcd70e6fc8452f8c5cad8f380
Frank Caron <frank@frankcaron.com>
2020-12-20 13:26:10 -0500

    Added Nav

--- force-app/main/default/lwc/propertyCard/propertyCard.js
+++ force-app/main/default/lwc/propertyCard/propertyCard.js
@@ -1,9 +1,22 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class PropertyCard extends LightningElement {
+export default class PropertyCard extends NavigationMixin(LightningElement) {
 
     //Variables
     @api
     property;
+
+    // Navigate to the record list page for the object
+    navigateToProperty() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.property.Id,
+                objectApiName: 'Rental_Property__c',
+                actionName: 'view'
+            }
+        });
+    }
 
 }
